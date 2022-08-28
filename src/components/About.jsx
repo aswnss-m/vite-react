697b1960bae7c38e599a2781e1ca29d893955abd
aswnss <aswnss.m@gmail.com>
2022-08-28 11:58:53 +0530

    skillset done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,58 +1,55 @@
 import "../assets/About.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHtml5, faCss3, faReact, faPython, faMarkdown, faGit,} from '@fortawesome/free-brands-svg-icons'
+import html from "../assets/logos/html.svg"
+import css from "../assets/logos/css.svg"
+import react from "../assets/logos/react.svg"
+import octocat from "../assets/logos/octocat.svg"
+import python from "../assets/logos/python.svg"
+import photoshop from "../assets/logos/photoshop.svg"
+import inkscape from "../assets/logos/inkscape.svg"
+import figma from "../assets/logos/figma.svg"
 
 function About() {
-    // const skills = {
-    //     faHtml5 : 'HTML',
-    //     faCss3 : 'CSS',
-    //     faReact:'React',
-    //     faPython:'Python',
-    //     faMarkdown:'Markdown',
-    //     faGit:'Git'
-    // };
+
     return (
-        <div className="about">
+        <div className="about"> 
             <h1>
                 About
             </h1>
             <div className="skills">
                 <h3>Skills</h3>
                 <div className="skillSet">
-                    {/* {Object.entries(skills).map(([key,value])=>(
-                        <span>
-                            
-                            <FontAwesomeIcon icon=key size="4x" />
-                            <p>{value}</p>
-                        </span>
-                    ))} */}
                     <span>
-                        <FontAwesomeIcon icon={faHtml5} size="3x" />
-                        <p>Html</p>
+                        <img src={html} alt="html5-logo" />
+                        {/* <htmllogo />  */}
+                        <p>HTML</p>
                     </span>
                     <span>
-                        <FontAwesomeIcon icon={faCss3} size="3x" />
+                        <img src={css} alt="css-logo" />
                         <p>CSS</p>
                     </span>
                     <span>
-                        <FontAwesomeIcon icon={faReact} size="3x" />
+                        <img src={react} alt="react-logo" />
                         <p>React</p>
                     </span>
                     <span>
-                        <FontAwesomeIcon icon={faMarkdown} size="3x" />
-                        <p>Markdown</p>
-                    </span>
-                    <span>
-                        <FontAwesomeIcon icon={faGit} size="3x" />
-                        <p>git</p>
-                    </span>
-                    <span>
-                        <FontAwesomeIcon icon={faPython} size="3x" />
+                        <img src={python} alt="python-logo" />
                         <p>Python</p>
                     </span>
                     <span>
-                        <FontAwesomeIcon icon={faMarkdown} size="3x" />
-                        <p>Markdown</p>
+                        <img src={octocat} alt="github-logo" />
+                        <p>git</p>
+                    </span>
+                    <span>
+                        <img src={photoshop} alt="photoshop-logo" />
+                        <p>Photoshop</p>
+                    </span>
+                    <span>
+                        <img src={inkscape} alt="inkscape-logo" />
+                        <p>Inkscape</p>
+                    </span>
+                    <span>
+                        <img src={figma} alt="figma-logo" />
+                        <p>Figma</p>
                     </span>
                 </div>
             </div>
