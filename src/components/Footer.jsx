6816cd99d596a6123195348430c378ca6ff13276
aswnss <aswnss.m@gmail.com>
2022-08-28 16:30:38 +0530

    footer section done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/Footer.css";
+function Footer() {
+    return(
+        <footer>
+            <p>
+            © 2022 - Template developed by aswnss
+            </p>
+        </footer>
+    )
+}
+export default Footer
